120b9de652ee38adc03184dd0068830351cccf08
Noppadol <nop_the_last_wizard@hotmail.com>
2021-05-30 18:06:39 +0700

    error msg for login page

--- login.php
+++ login.php
@@ -10,6 +10,26 @@
 			<input type="password" name="pwd" placeholder="Password...">
 			<button type="submit" name="submit">Log in</button>
 		</form>
+
+		<?php 
+			if (isset($_GET["error"])) {
+				if ($_GET["error"]=="emptyinput") {
+					echo "<p>Please fill all inputs field.</p>";
+				}
+				if ($_GET["error"]=="noaccount") {
+					echo "<p>Incorrect login information, No such an account.</p>";
+				}
+				if ($_GET["error"]=="wronglogin") {
+					echo "<p>Incorrect login information.</p>";
+				}
+				if ($_GET["error"]=="passwordincorrect") {
+					echo "<p>Incorrect password.</p>";
+				}
+				if ($_GET["error"]=="nousername") {
+					echo "<p>Incorrect username.</p>";
+				}				
+			}
+		?>
 	</section>
 </div>
 
